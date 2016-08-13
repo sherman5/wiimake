32686d1d0a759f474ac71096e84634022b3889df
Tom Sherman <tsherma4@jhu.edu>
2016-08-13 17:25:43 -0400

    added iterator for standard text file types

---
+++ src/TextFileParser.cpp
@@ -0,0 +1,84 @@
+#include <string>
+#include <cstdint>
+#include <fstream>
+#include <utility>
+
+#include "TextFileParser.h"
+
+TextFileParser::TextFileParser(std::string path, FileType type) {
+
+    m_file_type = type;
+    m_file_path = path;
+
+}
+
+TextFileParser::iterator TextFileParser::begin() {
+
+    return TextFileParser::iterator(this);
+
+}
+
+TextFileParser::iterator TextFileParser::end() {
+
+    return TextFileParser::iterator(this, true);
+
+}
+
+uint32_t TextFileParser::GetInjectionPoint() {
+
+    std::ifstream file_stream (m_file_path);
+    std::string addr;
+    file_stream >> addr;
+    return stoul(addr, nullptr, 16);
+
+}
+
+/********************* ITERATOR CLASS **********************/
+
+
+TextFileParser::iterator::iterator(TextFileParser* parser, bool end) {
+        
+    m_type = parser->m_file_type;
+    m_file_stream = new std::ifstream(parser->m_file_path);
+    *m_file_stream >> m_current_line;
+
+    if (m_type == regionFile) { *m_file_stream >> m_current_line;}
+
+    if (end) { while (*m_file_stream >> m_current_line) {} }
+
+}
+
+TextFileParser::iterator TextFileParser::iterator::operator++() {
+
+    *m_file_stream >> m_current_line;
+    return *this;
+
+}
+
+TextFileParser::iterator TextFileParser::iterator::operator++(int) {
+
+    iterator ret_iter = *this;
+    ++(*this);
+    return ret_iter;
+
+}
+
+bool TextFileParser::iterator::atEnd() {
+
+    return m_file_stream->eof();
+
+}
+
+MemSlot TextFileParser::iterator::operator*() {
+
+    std::string first, second;
+
+    first = m_current_line.substr(0, m_current_line.find('-'));
+    second = m_current_line.substr(m_current_line.find('-') + 1, m_current_line.length());
+
+    return std::make_pair(std::stoul(first, nullptr, 16), std::stoul(second, nullptr, 16));
+
+}
+
+/********************* END OF ITERATOR CLASS ***************/
+
--- src/TextFileParser.h
+++ src/TextFileParser.h
@@ -2,15 +2,60 @@
 #define TEXT_FILE_PARSER_H
 
 #include <string>
+#include <cstdint>
+#include <fstream>
+#include <utility>
 
-//address, value
-typedef std::pair<uint32_t, uint32_t> MEMslot;
+typedef std::pair<uint32_t, uint32_t> MemSlot;
+
+enum FileType {
+
+    injectionFile,
+    regionFile
+
+};
 
 class TextFileParser {
 
+    /********************* ITERATOR CLASS **********************/
+
+    public:
+
+    class iterator {
+
+    private:
+
+        FileType m_type;
+        std::string m_current_line;
+        std::ifstream* m_file_stream;
+
+    public:
+
+        iterator(TextFileParser*, bool = false);
+        //TODO: should have a destructor
+    
+        iterator operator++();
+        iterator operator++(int);
+        MemSlot operator*();
+        bool atEnd();
+
+    };
+
+    /********************* END OF ITERATOR CLASS ***************/
+
 private:
 
+    FileType m_file_type;
+    std::string m_file_path;
+
 public:
+
+    TextFileParser(std::string, FileType);
+
+    iterator begin();
+    iterator end();
+
+    uint32_t GetInjectionPoint();
 
 };
 
