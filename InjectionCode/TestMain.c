273568a779a61fa7f8fc8eb1316d682f16dabb44
Tom Sherman <tsherma4@jhu.edu>
2016-08-15 17:34:48 -0400

    added line to test function

--- InjectionCode/TestMain.c
+++ InjectionCode/TestMain.c
@@ -1,15 +1,15 @@
 #include <stdint.h>
 
 #include <TestLibrary1a.h>
-//#include "TestLibrary1b.h"
+#include <TestLibrary1b.h>
 #include <TestLibrary2.h>
 
 int _main(void) {
 
-//    *((uint32_t*) 0x80001800) = 0xABC;
-    *((uint32_t*) 0x80001800) = timesTwo(0x04);
-    *((uint32_t*) 0x80001804) = timesThree(0x04);
-    *((uint32_t*) 0x80001808) = timesFour(0x04);
-    *((float*) 0x8000180C) = 1.5f;
+    *((uint32_t*) 0x80001800) = 0xABC;
+    *((uint32_t*) 0x80001804) = timesTwo(0x04);
+    *((uint32_t*) 0x80001808) = timesThree(0x04);
+    *((uint32_t*) 0x8000180C) = timesFour(0x04);
+    *((float*) 0x80001810) = 1.5f;
 
 }
