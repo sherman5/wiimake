ca6a2dfe7c4f23ef101b026138b210ba1deb124f
Tom Sherman <tsherma4@jhu.edu>
2016-10-18 21:50:04 -0400

    comments

--- src/MemoryConfig.h
+++ src/MemoryConfig.h
@@ -19,6 +19,7 @@ public:
 
     private:
 
+        /* this class just wraps an internal iterator */        
         RegionList::iterator m_iter;
 
     public:
@@ -44,23 +45,38 @@ public:
 
 private:
 
+    /* address to inject code at */
     uint32_t m_inject_addr;
+
+    /* instruction being overridden at this address */
     uint32_t m_inject_instruction;
+
+    /* list of all available memory regions */
     RegionList m_regions;
 
 public:
 
+    /* default constructor */    
     MemoryConfig();
+
+    /* construct from file */    
     MemoryConfig(std::string);
 
-    void SetInjectAddress(std::string);
-    void SetInjectInstruction(std::string);
+    /* add memory region */    
     void AddRegion(std::string, std::string);
 
+    /* setters */
+    void SetInjectAddress(std::string);
+    void SetInjectInstruction(std::string);
+
+    /* getters */    
     uint32_t GetInjectAddress();
     uint32_t GetInjectInstruction();
 
+    /* get iterator to beginning of memory region list */
     iterator begin();
+
+    /* get iterator to end of memory region list */
     iterator end();
 
 };
