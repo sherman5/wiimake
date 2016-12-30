5f22b5ea0f7cb8e8810e08328570024b3f2a5ba5
Tom Sherman <tsherma4@jhu.edu>
2016-12-30 09:01:57 -0500

    fixed up examples

--- examples/ExampleMain.c
+++ examples/ExampleMain.c
@@ -1,14 +1,11 @@
 #include <stdint.h>
 
-//#include <mml/Controller.h>
 #include <mml/MeleeMath.h>
 
 #include "ExampleFunction.h"
 
-int _main(void) {
-
+int _main(void)
+{
     *((uint32_t*) 0x80001800) = TimesTwo(0x08);
-//    *((float*) 0x80001808) = mml_sin(30.0);
-//   Press('A');
-
+    *((float*) 0x80001808) = mml_sin(30.0);
 }
