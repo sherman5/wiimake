cc5c6ceca081eed832aeb62ab7fabdb8fad752f5
Tom Sherman <tsherma4@jhu.edu>
2016-12-30 20:06:50 -0500

    added new commands to help message

--- src/MainProgram/Description.h
+++ src/MainProgram/Description.h
@@ -20,45 +20,62 @@ namespace Description
     "\t injected at a specified address\n";
 
     const std::string usage = "\nUsage: "
-    "GCI [--mem-config <file>] [--iso-file <file>] [--save <file>]\n"
-    "\t [--load <file>] [--inject <directory>] [--lib <library>]\n"
-    "\t [--include <directory>] [--save-temps] [--ar <directory>]\n"
-    "\t [--output <output>] [--read <address>] [--help] [--version]\n"
-    "\t [--usage]\n";
+    "GCI [--inject <directory>] [--save <file>] [--load <file>]\n"
+    "\t [--ar <directory>] [--read <address>] [--config-file <file>]\n"
+    "\t [--game-id <id>] [--iso-file <file>] [--lib <library>]\n"
+    "\t [--include <directory>] [--output <filename>] [--save-temps]\n"
+    "\t [--help] [--usage] [--version]\n"
 
     const std::string space = "                            ";
 
-    const std::string help = "    --inject <directory>    "
+    const std::string help =
+    "    --inject <directory>    "
     "compile C code in <directory> and inject into iso\n"
     + space + "specified by --iso-file, only overwriting memory\n"
     + space + "regions specified by --mem-config\n"
-    "    --iso-file <file>       "
-    "specify which iso file to use\n"
-    "    --mem-config <file>     "
-    "specify a memory configuration file\n"
-    "    --include <directory>   "
-    "search for header files in <directory>\n"
-    "    --lib <library>         "
-    "link against <library>\n"
-    "    --save-temps            "
-    "don't delete temporary files created by injection\n"
-    + space + "process\n"
+    
+    "    --save <file>           "
+    "save state of iso from --iso-file to <file>\n"
+
+    "    --load <file>           "
+    "restore iso in --iso-file to state saved in <file>\n"
+
     "    --ar <directory>        "
     "compile C code in <directory> and create static\n"
     + space + "library\n"
-    "    --output <name>         "
-    "specify library name to be <output>\n"
-    "    --save <file>           "
-    "save state of iso from --iso-file to <file>\n"
-    "    --load <file>           "
-    "restore iso in --iso-file to state saved in <file>\n"
+
     "    --read <address>        "
     "read memory at <address> in iso file specified\n"
     + space + "with --iso-file\n"
+
+    "    --config-file <file>    "
+    "specify a the configuration file\n"
+
+    "    --game-id <id>          "
+    "id of game in the configuration file\n"
+
+    "    --iso-file <file>       "
+    "specify which iso file to use\n"
+
+    "    --lib <library>         "
+    "link against <library>\n"
+
+    "    --include <directory>   "
+    "search for header files in <directory>\n"
+
+    "    --output <filename>     "
+    "specify library name to be <output>\n"
+
+    "    --save-temps            "
+    "don't delete temporary files created by injection\n"
+    + space + "process\n"
+
     "    --help                  "
     "display this help message\n"
+
     "    --usage                 "
     "display usage message\n"
+
     "    --version               "
     "display program version\n";
 
